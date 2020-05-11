097dcf421fb45dd9531c01877a3dd2ffd6f8381a
Leonardo Daga <leonardo.daga@gmail.com>
2020-05-11 22:37:58 +0200

    mexw64 file format updated (removed msvcrtxxd.dll debug version dependency)
    Added a "compile all" matlab command

---
+++ Simulink/compile_all.m
@@ -0,0 +1,13 @@
+mex RS232_Buffer_Size.cpp -lRS232.lib
+mex RS232_Create_Buffer.cpp -lRS232.lib
+mex RS232_PeekB_Buffer.cpp -lRS232.lib
+mex RS232_ReadB_Buffer.cpp -lRS232.lib
+mex RS232_Read_Buffer.cpp -lRS232.lib
+mex RS232_Read_Fix_Format.cpp -lRS232.lib
+mex RS232_Read_Format.cpp -lRS232.lib
+mex RS232_Setup.cpp -lRS232.lib
+mex RS232_SynchB_Buffer.cpp -lRS232.lib
+mex RS232_Wait_Buffer_Synch.cpp -lRS232.lib
+mex RS232_WriteString.cpp -lRS232.lib
+mex RS232_Write_Binary.cpp -lRS232.lib
+mex RS232_Write_Format.cpp -lRS232.lib
